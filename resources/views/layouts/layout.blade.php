3a0f8287ea6fca9b46e231f7d87aaa3ca22d1999
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-07-24 19:20:33 +0545

    added main layout

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en-NP">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="{{ asset('svg/favicon.svg') }}" type="image/x-icon">
+    <title>@yield('title')</title>
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    @vite('resources/css/app.css')
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            /* border: 1px solid red; */
+        }
+
+        html {
+            line-height: 1.5;
+            -webkit-text-size-adjust: 100%;
+            -moz-tab-size: 4;
+            tab-size: 4;
+            font-family: Figtree, sans-serif;
+            font-feature-settings: normal
+        }
+    </style>
+</head>
+
+<body>
+
+</body>
+
+</html>
