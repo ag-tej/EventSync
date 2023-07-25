b0fadd04c550fba9cfc92a1ccf21e348dd396202
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-07-25 14:58:36 +0545

    added navigation bars

---
+++ resources/views/components/explore_navbar.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar">
+    <aside class="flex items-center space-x-8">
+        <a href="/" class="flex items-center space-x-1 text-2xl font-mono">
+            <img alt="logo" src="/svg/favicon.svg" width="25" decoding="async" loading="lazy">
+            <p class="text-white">EventSync</p>
+        </a>
+        <a href="#" class="hover:text-white">Academic</a>
+        <a href="#" class="hover:text-white">Business</a>
+        <a href="#" class="hover:text-white">Social</a>
+        <a href="#" class="hover:text-white">Community</a>
+        <a href="#" class="hover:text-white">Recreational</a>
+    </aside>
+    <aside class="flex items-center space-x-8 font-semibold">
+        <p class="hover:text-white cursor-pointer" onclick="document.getElementById('sign_in').className ='block'">
+            Sign in
+        </p>
+        <p class="text-[#ffa4f2] cursor-pointer" onclick="document.getElementById('sign_up').className ='block'">
+            Get Started
+        </p>
+    </aside>
+</nav>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar">
+    <aside class="flex items-center space-x-8">
+        <a href="/" class="flex items-center space-x-1 text-2xl font-mono">
+            <img alt="logo" src="/svg/favicon.svg" width="25" decoding="async" loading="lazy">
+            <p class="text-white">EventSync</p>
+        </a>
+        <a href="/explore" class="hover:text-white">Explore</a>
+        <a href="#" class="hover:text-white">Features</a>
+        <a href="#" class="hover:text-white">FAQ's</a>
+        <a href="#" class="hover:text-white">Blog</a>
+    </aside>
+    <aside class="flex items-center space-x-8 font-semibold">
+        <p class="hover:text-white cursor-pointer" onclick="document.getElementById('sign_in').className ='block'">
+            Sign in
+        </p>
+        <p class="text-[#ffa4f2] cursor-pointer" onclick="document.getElementById('sign_up').className ='block'">
+            Get Started
+        </p>
+    </aside>
+</nav>
