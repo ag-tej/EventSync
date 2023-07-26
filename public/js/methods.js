948a309d50028e480b12c383cf2b28237a6214bf
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-07-26 12:02:54 +0545

    updated loading function

--- public/js/methods.js
+++ public/js/methods.js
@@ -1,8 +1,8 @@
 // loading svg icon on click
 function loading() {
-    document.getElementById('create').disabled = true;
+    document.getElementById('button').disabled = true;
     const loading = document.getElementById('loading_icon')
     loading.classList.toggle('hidden')
     loading.classList.toggle('inline')
-    document.getElementById('submitForm').submit();
+    document.getElementById('form').submit();
 }
