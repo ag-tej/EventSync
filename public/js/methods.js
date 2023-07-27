b09235da1315e5da465d24b55617c35bf22c1e1c
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-07-27 23:41:51 +0545

    removed userDropdown function

--- public/js/methods.js
+++ public/js/methods.js
@@ -6,19 +6,3 @@ function loading() {
     loading.classList.toggle('inline')
     document.getElementById('form').submit();
 }
-
-// user dropdown toggle
-function userDropdown(elementId1, elementId2) {
-    var dropdown1 = document.getElementById(elementId1);
-    const isVisible1 = dropdown1.style.visibility !== 'hidden';
-    if (isVisible1)
-        dropdown1.style.visibility = 'hidden';
-    else
-        dropdown1.style.visibility = 'visible';
-    var dropdown2 = document.getElementById(elementId2);
-    const isVisible2 = dropdown2.style.visibility !== 'hidden';
-    if (isVisible2)
-        dropdown2.style.visibility = 'hidden';
-    else
-        dropdown2.style.visibility = 'visible';
-}
