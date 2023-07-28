32f46734505bdb7302db516d40a52818b43fa750
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-07-28 12:37:05 +0545

    added aos library

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -8,6 +8,8 @@
     <link rel="shortcut icon" href="{{ asset('svg/favicon.svg') }}" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script src="{{ asset('js/methods.js') }}" defer></script>
     <title>@yield('title')</title>
     @vite('resources/css/app.css')
@@ -17,6 +19,9 @@
 </head>
 
 <body class="bg-[#f8f8f8]">
+    <script>
+        AOS.init();
+    </script>
     @include('components.toast_notification')
     @yield('content')
 </body>
