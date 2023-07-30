9b8f0b9430061404a0c731f1ecc09e1589ece4dc
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-07-30 14:20:48 +0545

    added flowbite library

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,10 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="{{ asset('svg/favicon.svg') }}" type="image/x-icon">
+    {{-- figtree font --}}
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    {{-- aos animation --}}
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    {{-- flowbite --}}
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.0/flowbite.min.css" rel="stylesheet" />
     <script src="{{ asset('js/methods.js') }}" defer></script>
     <title>@yield('title')</title>
     @vite('resources/css/app.css')
@@ -24,6 +28,8 @@
     </script>
     @include('components.toast_notification')
     @yield('content')
+    {{-- flowbite --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.0/flowbite.min.js"></script>
 </body>
 
 </html>
