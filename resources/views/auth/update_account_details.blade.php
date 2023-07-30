e5d375961ab00226ae36cc654982cb952342426a
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-07-30 22:34:33 +0545

    added forms

---
+++ resources/views/auth/update_account_details.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.layout')
+
+@section('title')
+    Update Account Details | EventSync
+@endsection
+
+@section('content')
+    @include('components.explore_navbar')
+
+    {{-- update account details form --}}
+    <a href="{{ route('settings') }}" class="cursor-default">
+        <div class="backdrop">
+        </div>
+    </a>
+    <div class="popup_center">
+        <div data-aos="zoom-in" data-aos-duration="300" class="popup">
+            <p class="mb-4 font-bold text-lg">Update Account Details</p>
+            <form action="{{ route('user-profile-information.update') }}" method="POST" enctype="multipart/form-data"
+                id="form">
+                @csrf
+                @method('PUT')
+                <div class="mb-4">
+                    <label for="username" class="form_label">Username</label>
+                    <input type="text" id="username" name="username" class="form_input"
+                        value="{{ Auth::user()->username }}">
+                    @error('username')
+                        <script>
+                            document.getElementById('username').classList.add('border-red-500')
+                        </script>
+                        <p class="text-sm text-red-500">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-4">
+                    <label for="email" class="form_label">Email address</label>
+                    <input type="email" id="email" name="email" class="form_input"
+                        value="{{ Auth::user()->email }}">
+                    @error('email')
+                        <script>
+                            document.getElementById('email').classList.add('border-red-500')
+                        </script>
+                        <p class="text-sm text-red-500">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="flex space-x-5">
+                    <a href="{{ route('settings') }}" class="button w-full bg-gray-500 hover:shadow-gray-500/30">
+                        Cancel
+                    </a>
+                    <button type="submit" class="button w-full" id="button" onclick="loading()">
+                        <img aria-hidden="true" src="{{ asset('svg/loading_icon.svg') }}"
+                            class="hidden w-4 h-4 mr-2 animate-spin" id="loading_icon">
+                        Update
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/auth/update_password.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.layout')
+
+@section('title')
+    Update Password | EventSync
+@endsection
+
+@section('content')
+    @include('components.explore_navbar')
+
+    {{-- update password form --}}
+    <a href="{{ route('settings') }}" class="cursor-default">
+        <div class="backdrop">
+        </div>
+    </a>
+    <div class="popup_center">
+        <div data-aos="zoom-in" data-aos-duration="300" class="popup">
+            <p class="mb-4 font-bold text-lg">Update Password</p>
+            <form action="{{ route('user-password.update') }}" method="POST" enctype="multipart/form-data" id="form">
+                @csrf
+                @method('PUT')
+                <div class="mb-4">
+                    <label for="current_password" class="form_label">Current Password</label>
+                    <input type="password" id="current_password" name="current_password" class="form_input">
+                    @error('current_password')
+                        <script>
+                            document.getElementById('current_password').classList.add('border-red-500')
+                        </script>
+                        <p class="text-sm text-red-500"><small>{{ $message }}</small></p>
+                    @enderror
+                </div>
+                <div class="mb-4">
+                    <label for="password" class="form_label">New Password</label>
+                    <input type="password" id="password" name="password" class="form_input">
+                    @error('password')
+                        <script>
+                            document.getElementById('password').classList.add('border-red-500')
+                        </script>
+                        <p class="text-sm text-red-500"><small>{{ $message }}</small></p>
+                    @enderror
+                </div>
+                <div class="mb-4">
+                    <label for="password_confirmation" class="form_label">Confirm New Password</label>
+                    <input type="password" id="password_confirmation" name="password_confirmation" class="form_input">
+                    @error('password_confirmation')
+                        <script>
+                            document.getElementById('password_confirmation').classList.add('border-red-500')
+                        </script>
+                        <p class="text-sm text-red-500"><small>{{ $message }}</small></p>
+                    @enderror
+                </div>
+                <div class="flex space-x-5">
+                    <a href="{{ route('settings') }}" class="button w-full bg-gray-500 hover:shadow-gray-500/30">
+                        Cancel
+                    </a>
+                    <button type="submit" class="button w-full" id="button" onclick="loading()">
+                        <img aria-hidden="true" src="{{ asset('svg/loading_icon.svg') }}"
+                            class="hidden w-4 h-4 mr-2 animate-spin" id="loading_icon">
+                        Update
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
