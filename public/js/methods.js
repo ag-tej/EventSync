ac3f3bb1087d4cab924af9b40122498b9b450a21
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-08-02 21:37:03 +0545

    added live image preview function

--- public/js/methods.js
+++ public/js/methods.js
@@ -1,3 +1,19 @@
+// live image preview
+function previewImage(event) {
+    var reader = new FileReader();
+    reader.onload = function () {
+        var preview = document.getElementById('preview');
+        preview.classList.toggle('hidden')
+        preview.classList.toggle('block')
+        preview.src = reader.result;
+        var oldImage = document.getElementById('oldImage');
+        if (oldImage) {
+            oldImage.className = 'hidden';
+        }
+    }
+    reader.readAsDataURL(event.target.files[0]);
+}
+
 // loading svg icon on click
 function loading() {
     document.getElementById('button').disabled = true;
